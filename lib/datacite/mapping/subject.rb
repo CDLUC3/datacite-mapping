1c121c0e1a98ce8144bdbc60981f357b7a42a27b
David Moles <david.moles@ucop.edu>
2015-12-01 15:09:37 -0800

    Use :language instead of :lang for consistency

--- lib/datacite/mapping/subject.rb
+++ lib/datacite/mapping/subject.rb
@@ -10,18 +10,18 @@ module Datacite
       text_node :_lang, '@xml:lang', default_value: nil
       text_node :value, 'text()'
 
-      def initialize(scheme: nil, scheme_uri: nil, lang:, value:)
+      def initialize(scheme: nil, scheme_uri: nil, language:, value:)
         self.scheme = scheme
         self.scheme_uri = scheme_uri
-        self._lang = lang
+        self.language = language
         self.value = value
       end
 
-      def lang
+      def language
         _lang || 'en'
       end
 
-      def lang=(value)
+      def language=(value)
         fail ArgumentError, 'Language cannot be nil' unless value
         self._lang = value
       end
