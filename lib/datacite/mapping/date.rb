c8c8dcd87c894d693102f7e591a09dd6aa308103
David Moles <david.moles@ucop.edu>
2015-12-07 12:27:02 -0800

    More documentation

--- lib/datacite/mapping/date.rb
+++ lib/datacite/mapping/date.rb
@@ -2,10 +2,31 @@ require 'xml/mapping_extensions'
 
 module Datacite
   module Mapping
+    # Represents a DataCite `<date/>` field, which can be a year, date (year-month-day or just year-month),
+    # or ISO8601 datetime.
+    #
+    # @!attribute [r] year
+    #   @return [Integer] The year.
+    # @!attribute [r] month
+    #   @return [Integer, nil] The month. Can be `nil` if no month was specified.
+    # @!attribute [r] day
+    #   @return [Integer, nil] The day. Can be `nil` if no day was specified.
+    # @!attribute [r] hour
+    #   @return [Integer, nil] The hour. Can be `nil` if no hour was specified.
+    # @!attribute [r] minute
+    #   @return [Integer, nil] The minutes. Can be `nil` if no minutes were specified.
+    # @!attribute [r] sec
+    #   @return [Integer, nil] The seconds. Can be `nil` if no seconds were specified.
+    # @!attribute [r] nsec
+    #   @return [Integer, nil] The nanoseconds. Can be `nil` if no nanoseconds were specified.
     class Date
       include XML::Mapping
 
-      text_node :_value, 'text()'
+      # @!method value
+      #   @return [String] The value as a string. May be any [W3C DateTime format](http://www.w3.org/TR/NOTE-datetime).
+      text_node :value, 'text()'
+      alias_method :_value=, :value=
+      private :_value=
 
       attr_reader :year
       attr_reader :month
@@ -15,10 +36,18 @@ module Datacite
       attr_reader :sec
       attr_reader :nsec
 
+      # Initializes a new `Date`
+      #
+      # @param value [DateTime, Date, Integer, String] The value, as a `DateTime`, `Date`, or `Integer`,
+      #   or as a `String` in any [W3C DateTime format](http://www.w3.org/TR/NOTE-datetime)
       def initialize(value:)
         self.value = value
       end
 
+      # Sets the value.
+      #
+      # @param val [DateTime, Date, Integer, String] The value, as a `DateTime`, `Date`, or `Integer`,
+      #   or as a `String` in any [W3C DateTime format](http://www.w3.org/TR/NOTE-datetime)
       def value=(val) # rubocop:disable Metrics/MethodLength, Metrics/AbcSize, Metrics/CyclomaticComplexity, Metrics/PerceivedComplexity
         @date_time = to_datetime(val)
         @date = @date_time ? @date_time.to_date : to_date(val)
@@ -34,10 +63,6 @@ module Datacite
         end
         fail ArgumentError, "Unable to parse date value '#{val}'" unless @year
         self._value = new_value
-      end
-
-      def value
-        _value
       end
 
       private
