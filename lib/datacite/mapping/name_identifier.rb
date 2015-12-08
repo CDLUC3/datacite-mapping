ec443e21df516711a5d8dff64fd5855c75404e3b
David Moles <david.moles@ucop.edu>
2015-12-08 11:16:38 -0800

    Added docs and error checks

--- lib/datacite/mapping/name_identifier.rb
+++ lib/datacite/mapping/name_identifier.rb
@@ -2,19 +2,42 @@ require 'xml/mapping_extensions'
 
 module Datacite
   module Mapping
+    # Uniquely identifies an individual or legal entity, according to various schemes.
     class NameIdentifier
       include XML::Mapping
 
       root_element_name 'nameIdentifier'
 
+      # @!attribute [rw] scheme
+      #   @return [URI] the name identifier scheme. Cannot be nil.
       text_node :scheme, '@nameIdentifierScheme'
+      # @!attribute [rw] scheme_uri
+      #   @return [URI, nil] the URI of the identifier scheme. Optional.
       uri_node :scheme_uri, '@schemeURI', default_value: nil
+      # @!attribute [rw] value
+      #   @return [String] the identifier value. Cannot be nil.
       text_node :value, 'text()'
+
+      alias_method :_scheme=, :scheme=
+      private :_scheme=
+
+      alias_method :_value=, :value=
+      private :_value=
 
       def initialize(scheme:, scheme_uri: nil, value:)
         self.scheme = scheme
         self.scheme_uri = scheme_uri
         self.value = value
+      end
+
+      def scheme=(v)
+        fail ArgumentError, 'Scheme cannot be empty or nil' unless v && !v.empty?
+        self._scheme = v
+      end
+
+      def value=(v)
+        fail ArgumentError, 'Value cannot be empty or nil' unless v && !v.empty?
+        self._value = v
       end
     end
   end
--- spec/unit/datacite/mapping/name_identifier_spec.rb
+++ spec/unit/datacite/mapping/name_identifier_spec.rb
@@ -20,6 +20,50 @@ module Datacite
           id = NameIdentifier.new(scheme: 'ORCID', value: '0000-0003-1632-8708')
           expect(id.scheme_uri).to be_nil
         end
+
+        it 'requires a scheme' do
+          expect { NameIdentifier.new(value: '0000-0003-1632-8708') }.to raise_error(ArgumentError)
+        end
+
+        it 'requires a non-empty scheme' do
+          expect { NameIdentifier.new(scheme: '', value: '0000-0003-1632-8708') }.to raise_error(ArgumentError)
+        end
+
+        it 'requires a value' do
+          expect { NameIdentifier.new(scheme: 'ORCID') }.to raise_error(ArgumentError)
+        end
+
+        it 'requires a non-empty value' do
+          expect { NameIdentifier.new(scheme: '', scheme: 'ORCID') }.to raise_error(ArgumentError)
+        end
+      end
+
+      describe '#scheme=' do
+        it 'requires a scheme' do
+          id = NameIdentifier.new(scheme: 'ORCID', value: '0000-0003-1632-8708')
+          expect { id.scheme = nil }.to raise_error(ArgumentError)
+          expect(id.scheme).to eq('ORCID')
+        end
+
+        it 'requires a non-empty scheme' do
+          id = NameIdentifier.new(scheme: 'ORCID', value: '0000-0003-1632-8708')
+          expect { id.scheme = '' }.to raise_error(ArgumentError)
+          expect(id.scheme).to eq('ORCID')
+        end
+      end
+
+      describe '#value=' do
+        it 'requires a value' do
+          id = NameIdentifier.new(scheme: 'ORCID', value: '0000-0003-1632-8708')
+          expect { id.value = nil }.to raise_error(ArgumentError)
+          expect(id.value).to eq('0000-0003-1632-8708')
+        end
+
+        it 'requires a non-empty value' do
+          id = NameIdentifier.new(scheme: 'ORCID', value: '0000-0003-1632-8708')
+          expect { id.value = '' }.to raise_error(ArgumentError)
+          expect(id.value).to eq('0000-0003-1632-8708')
+        end
       end
 
       describe '#load_from_xml' do
