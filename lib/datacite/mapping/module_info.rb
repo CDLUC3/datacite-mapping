9d755b852cfc7b4fdac7eb3657f2935b5b754b2a
David Moles <david.moles@ucop.edu>
2016-01-06 12:21:41 -0800

    Bump version and copyright year

--- lib/datacite/mapping/module_info.rb
+++ lib/datacite/mapping/module_info.rb
@@ -4,9 +4,9 @@ module Datacite
     NAME = 'datacite-mapping'
 
     # The version of this gem
-    VERSION = '0.1.3'
+    VERSION = '0.1.4'
 
     # The copyright notice for this gem
-    COPYRIGHT = 'Copyright (c) 2015 The Regents of the University of California'
+    COPYRIGHT = 'Copyright (c) 2016 The Regents of the University of California'
   end
 end
