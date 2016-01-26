13262d400effb5c19515a49655d0de010fdb8cd7
David Moles <david.moles@ucop.edu>
2016-01-26 15:34:03 -0800

    More require debugging

--- lib/datacite/mapping.rb
+++ lib/datacite/mapping.rb
@@ -1,3 +1,14 @@
+unless $debug_require
+  def require(path)
+    result = super
+    msg = "require(#{path}) => #{result} (#{caller[0]})"
+    File.open('/tmp/require.log', 'a') { |f| f.puts(msg) }
+    puts(msg)
+    result
+  end
+  $debug_require = true
+end
+
 require 'logger'
 
 File.open('/tmp/require.log', 'a') do |f|
