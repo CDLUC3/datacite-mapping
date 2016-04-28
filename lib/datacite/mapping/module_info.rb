2126eedca37a5cfa17e07fd9e28e0ff29c7a5c93
David Moles <david.moles@ucop.edu>
2016-04-28 16:18:06 -0700

    Bump version due to bad gem push

--- lib/datacite/mapping/module_info.rb
+++ lib/datacite/mapping/module_info.rb
@@ -4,7 +4,7 @@ module Datacite
     NAME = 'datacite-mapping'
 
     # The version of this gem
-    VERSION = '0.1.11'
+    VERSION = '0.1.12'
 
     # The copyright notice for this gem
     COPYRIGHT = 'Copyright (c) 2016 The Regents of the University of California'
