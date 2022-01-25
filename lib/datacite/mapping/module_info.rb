b301e54bee1856844a6bc6c3e9696d8f885b42bf
Scott Fisher <scott.fisher@ucop.edu>
2022-01-25 15:57:59 -0800

    updating datacite mapping gem

--- lib/datacite/mapping/module_info.rb
+++ lib/datacite/mapping/module_info.rb
@@ -6,9 +6,9 @@ module Datacite
     NAME = 'datacite-mapping'
 
     # The version of this gem
-    VERSION = '0.4.0'
+    VERSION = '0.4.1'
 
     # The copyright notice for this gem
-    COPYRIGHT = 'Copyright (c) 2019 The Regents of the University of California'
+    COPYRIGHT = 'Copyright (c) 2022 The Regents of the University of California'
   end
 end
