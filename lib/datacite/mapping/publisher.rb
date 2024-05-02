578c8d2c43ab494c52b3620ad08e764bb154b1ec
Audrey Hamelers <hamelers@ebi.ac.uk>
2024-05-02 22:52:24 +0200

    add publisher identifier and scheme (datacite 4.5)

--- lib/datacite/mapping/publisher.rb
+++ lib/datacite/mapping/publisher.rb
@@ -14,11 +14,20 @@ module Datacite
       # @param value [String] name of the publisher
       def initialize(value:, language: nil)
         self.language = language
+        self.identifier = identifier
+        self.scheme = scheme
+        self.scheme_uri = scheme_uri
         self.value = value
       end
 
       def language=(value)
         @language = value&.strip
+      end
+
+      def scheme=(new_value)
+        raise ArgumentError, 'Scheme cannot be empty or nil' unless new_value && !new_value.empty?
+
+        @scheme = new_value
       end
 
       def value=(value)
@@ -31,6 +40,18 @@ module Datacite
       # @!attribute [rw] language
       #   @return [String] an IETF BCP 47, ISO 639-1 language code identifying the language.
       text_node :language, '@xml:lang', default_value: nil
+
+      # @!attribute [rw] identifier
+      #   @return [NameIdentifier, nil] an identifier for the contributor. Optional.
+      object_node :identifier, '@publisherIdentifier', default_value: nil
+
+      # @!attribute [rw] scheme
+      #   @return [String] the name identifier scheme. Cannot be nil.
+      text_node :scheme, '@publisherIdentifierScheme'
+
+      # @!attribute [rw] scheme_uri
+      #   @return [URI, nil] the URI of the identifier scheme. Optional.
+      uri_node :scheme_uri, '@schemeURI', default_value: nil
 
       # @!attribute [rw] value
       #   @return [String] the title itself.
